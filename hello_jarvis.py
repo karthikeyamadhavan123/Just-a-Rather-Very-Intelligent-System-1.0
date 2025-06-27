28f28279ca9247468963c51c84c9c324afb6846f
karthikeyamadhavan123 <karthikeyadonnipad2005@gmail.com>
2025-06-27 22:30:46 +0530

    basic help commands completed

--- hello_jarvis.py
+++ hello_jarvis.py
@@ -1,8 +1,8 @@
+
 from datetime import datetime
 print("Hello I am jarvis")
 name=input("What is your name?:")
-print(f"Nice to meet you {name}! How can I help you today?")
-
+print(f"Nice to meet you {name}! How can I help you today?") 
 while True:
      prompt = input('How may i help you?')
      match prompt.lower():
@@ -10,9 +10,8 @@ while True:
                   now = datetime.now()
                   current_time=now.strftime("%H:%M:%S")
                   print(f"current time is {current_time}")
-          case "help":
-                 print("What you need to help?")
+          case "--help":
+                 f =open("help.txt",'r',encoding='utf-8')
+                 print(f.read())
           case "goodbye":
                  quit()       
-         
-     
